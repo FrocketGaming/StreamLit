5f2d4b4128b73eb9ae962b5af1f13b6e957ed914
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2022-11-03 17:44:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import streamlit as st
 import PyQt5
 
 
-def format_clipboard_query(paste):
-    return ", ".join(repr(s) for s in paste.split("\r\n"))
+def format_clipboard_query():
+    return ", ".join(repr(s) for s in pyperclip.paste().split("\r\n"))
 
 
-paste = st.text_input('Paste Your Values', '')
-
-if st.button('Copy'):
-    format_clipboard_query(paste)
+if st.button('Copy to Clipboard'):
+    format_clipboard_query()
