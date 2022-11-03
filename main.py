064f784d1dadf4ac83a06e357288e6618bc43c26
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2022-11-03 18:13:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def format_clipboard_query(value):
     return ", ".join(repr(s) for s in value.split("\n"))
 
 
-value = st.text_area('Enter values to modify')
+value = st.text_area('Enter values to modify', height=500)
 
 if st.button('Modify'):
     st.write(format_clipboard_query(value))
