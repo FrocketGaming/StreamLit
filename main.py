a04e6f14ec88531d7ae6b495a67b444a8d8698da
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2022-11-03 16:34:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pyperclip
+import streamlit as st
 
 
 def format_clipboard_query():
     return ", ".join(repr(s) for s in pyperclip.paste().split("\r\n"))
 
 
-pyperclip.copy(format_clipboard_query())
+if st.button('Covert Clipboard'):
+    format_clipboard_query()
+    st.write("Done!")
