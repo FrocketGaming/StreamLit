0ecf05bad8653b09fe07019716beaf3153b7ea24
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2022-11-17 15:54:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ selected = option_menu(
     menu_title=None,
     options=["SQL Formatter", "JSON Formatter",
              "Ftr Feature"],
-    icons=["code-square", "bi-filetype-json", "bricks"],
+    icons=["code-square", "filetype json", "bricks"],
     menu_icon="cast",
     default_index=0,
     orientation="horizontal",
