536be319af2ccd2d6e18ab25f9f4ca177cb5a084
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2022-12-06 12:06:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -68,24 +68,14 @@ def pretty_xml(user_text):
 
 def sql_data_format(user_text, checked):
     if checked == True:
-        if ', ' in user_text:
-            updated_text = "(" + ", ".join(repr(s)
-                                           for s in user_text.replace(',', "").split()) + ")"
-            return textwrap.fill(updated_text, 65)
-        else:
-            updated_text = "(" + ",".join(repr(s)
-                                          for s in user_text.replace(',', "").split()) + ")"
-            return textwrap.fill(updated_text, 65)
+        updated_text = "(" + ", ".join(repr(s)
+                                       for s in user_text.replace(',', "").split()) + ")"
+        return textwrap.fill(updated_text, 65)
 
     elif checked == False:
-        if ', ' in user_text:
-            updated_text = ", ".join(
-                repr(s) for s in user_text.replace(',', "").split())
-            return textwrap.fill(updated_text, 65)
-        else:
-            updated_text = ",".join(epr(s)
-                                    for s in user_text.replace(',', "").split())
-            return textwrap.fill(updated_text, 65)
+        updated_text = ", ".join(
+            repr(s) for s in user_text.replace(',', "").split())
+        return textwrap.fill(updated_text, 65)
 
 
 def java_extract(user_text):
