08f515c5813ab545b32df8adb8241aded4693e99
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2023-03-21 16:12:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def java_extract(user_text):
         new_text = """"""
 
         for line in user_text.splitlines():
-            new_text += re.sub(r'[^"]*"([^"]*)"', r"\1", line)
+            new_text += re.sub(r'[^"]*"([^"]*)"', r"$1", line)
 
     return new_text
 
