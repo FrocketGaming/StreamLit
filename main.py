088af00e722d96b6ae865b3f5d33ca4965f1b1cf
FrocketGaming <74742645+FrocketGaming@users.noreply.github.com>
2023-03-21 16:42:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,8 @@ def java_extract(user_text):
         new_text = """"""
 
         for line in user_text.splitlines():
-            new_text += re.sub('(String sql = ")|(?:\+ ")|(?:" \+)', '', line)
+            new_text += re.sub(
+                '(String sql = ")|(?:\+ ")|(?:" \+)|["]', '', line)
 
     return new_text
 
