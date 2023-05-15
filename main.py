d5f792aa4ab292a8c8ec5b881529b1a46d259792
Chris <74742645+FrocketGaming@users.noreply.github.com>
2023-05-15 12:15:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,32 +8,30 @@ from st_btn_select import st_btn_select
 import textwrap
 import xml.etree.ElementTree as ET
 
-st.set_page_config(
-    page_title="SQL Formatter",
-    page_icon=":shark:")
+st.set_page_config(page_title="SQL Formatter", page_icon=":shark:")
 
 
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 local_css("streamlit.css")
 
 selected = option_menu(
     menu_title=None,
-    options=["SQL Formatter", "JSON Formatter",
-             "XML Formatter", "CRON Placeholder"],
+    options=["SQL Formatter", "JSON Formatter", "XML Formatter", "CRON Placeholder"],
     icons=["code", "braces", "bricks"],
     menu_icon="cast",
     default_index=0,
     orientation="horizontal",
     styles={
-        "container": {"padding": "0px",
-                      "display": "grid",
-                      "margin": "0!important",
-                      "background-color": "#212121"
-                      },
+        "container": {
+            "padding": "0px",
+            "display": "grid",
+            "margin": "0!important",
+            "background-color": "#212121",
+        },
         "icon": {"color": "#bd93f9", "font-size": "14px"},
         "nav-link": {
             "font-size": "14px",
@@ -43,15 +41,15 @@ selected = option_menu(
             "height": "30px",
             "width": "13rem",
             "color": "#edff85",
-            "border-radius": "5px"
+            "border-radius": "5px",
         },
         "nav-link-selected": {
             "background-color": "#212121",
             "font-weight": "300",
             "color": "#f7f8f2",
-            "border": "1px solid #e55381"
-        }
-    }
+            "border": "1px solid #e55381",
+        },
+    },
 )
 
 
@@ -63,24 +61,25 @@ def pretty_json(text):
 def pretty_xml(user_text):
     new_text = ET.XML(user_text)
     ET.indent(new_text)
-    return ET.tostring(new_text, encoding='unicode')
+    return ET.tostring(new_text, encoding="unicode")
 
 
 def sql_data_format(user_text, checked):
     if checked == True:
-        updated_text = "(" + ", ".join(repr(s)
-                                       for s in user_text.replace(',', "").split()) + ")"
+        updated_text = (
+            "(" + ", ".join(repr(s) for s in user_text.replace(",", "").split()) + ")"
+        )
         return textwrap.fill(updated_text, 65)
 
     elif checked == False:
-        if ',' in user_text or ', ' in user_text:
-            user_text.replace(',', ' ')
-            updated_text = ','.join(
-                repr(s) for s in user_text.split(','))
+        if "," in user_text or ", " in user_text:
+            user_text.replace(",", " ")
+            updated_text = ",".join(repr(s) for s in user_text.split(","))
             return textwrap.fill(updated_text, 65)
-        elif not ', ' in user_text or ',' in user_text:
+        elif not ", " in user_text or "," in user_text:
             updated_text = ", ".join(
-                repr(s) for s in user_text.replace(',', "").split())
+                repr(s) for s in user_text.replace(",", "").split()
+            )
             return textwrap.fill(updated_text, 65)
 
 
@@ -88,7 +87,8 @@ def java_extract(user_text):
     new_text = """"""
 
     new_text += re.sub(
-        '(String sql = ")|^.*(append.")|(?:\+ ")|(?:" \+)|["]', '', user_text)
+        '(String sql = ")|^.*(append.")|(?:\+ ")|(?:" \+)|["]', "", user_text
+    )
 
     return new_text
 
@@ -96,23 +96,20 @@ def java_extract(user_text):
 def xml_formatter():
     st.header("XML Formatter")
 
-    st.caption('Paste the XML data below to format')
+    st.caption("Paste the XML data below to format")
 
     column = st.columns(2)
 
-    with column[1]:
-        placeholder = st.empty()
-        with placeholder.container():
-            st.code('')
-
     with column[0]:
         user_text = st.text_area(
-            'xml', height=450, placeholder="Paste here", label_visibility='hidden')
+            "xml", height=450, placeholder="Paste here", label_visibility="hidden"
+        )
 
-        if st.button('Format'):
+        if st.button("Format"):
             try:
+                placeholder = st.empty()
                 with placeholder.container():
-                    st.code(pretty_xml(user_text), language='xml')
+                    st.code(pretty_xml(user_text), language="xml")
             except:
                 st.text("Error: Please provide xml data.")
 
@@ -120,23 +117,20 @@ def xml_formatter():
 def json_formatter():
     st.header("JSON Formatter")
 
-    st.caption('Paste the JSON data below to format')
+    st.caption("Paste the JSON data below to format")
 
     column = st.columns(2)
 
-    with column[1]:
-        placeholder = st.empty()
-        with placeholder.container():
-            st.code('')
-
     with column[0]:
         user_text = st.text_area(
-            'json', height=450, placeholder="Paste here", label_visibility='hidden')
+            "json", height=450, placeholder="Paste here", label_visibility="hidden"
+        )
 
-        if st.button('Format'):
+        if st.button("Format"):
             try:
+                placeholder = st.empty()
                 with placeholder.container():
-                    st.code(pretty_json(user_text), language='json')
+                    st.code(pretty_json(user_text), language="json")
             except:
                 st.text("Error: Please provide JSON data.")
 
@@ -144,41 +138,43 @@ def json_formatter():
 def column_formatter():
     st.header("SQL Formatter")
 
-    st.caption("Enter a list of items to format for a SQL query, an entire query to format for readability,\nor java code containing a query to remove the java and format the query")
+    st.caption(
+        "Enter a list of items to format for a SQL query, an entire query to format for readability,\nor java code containing a query to remove the java and format the query"
+    )
     column = st.columns(2)
 
-    with column[1]:
-        placeholder = st.empty()
-        with placeholder.container():
-            st.code('')
-
     with column[0]:
-
         user_text = st.text_area(
-            'txt', height=450, placeholder='Paste here', label_visibility='hidden')
+            "txt", height=450, placeholder="Paste here", label_visibility="hidden"
+        )
 
         checked = False
         if st.checkbox("Add Parathesis (Only for Format Data)"):
             checked = True
 
         selection = st.radio(
-            'None', ("Format Data", "Format Query", "Remove Java"), label_visibility='hidden')
+            "None",
+            ("Format Data", "Format Query", "Remove Java"),
+            label_visibility="hidden",
+        )
 
-    if st.button('Run'):
+    if st.button("Run"):
         with column[1]:
             if selection == "Format Data":
                 try:
+                    placeholder = st.empty()
                     with placeholder.container():
-                        st.code(sql_data_format(
-                            user_text, checked), language='sql')
+                        st.code(sql_data_format(user_text, checked), language="sql")
                 except:
                     st.text("Error: Please provide data for formatting.")
             elif selection == "Remove Java":
+                placeholder = st.empty()
                 with placeholder.container():
-                    st.code(format_sql(java_extract(user_text)), language='sql')
+                    st.code(format_sql(java_extract(user_text)), language="sql")
             elif selection == "Format Query":
+                placeholder = st.empty()
                 with placeholder.container():
-                    st.code(format_sql(user_text), language='sql')
+                    st.code(format_sql(user_text), language="sql")
 
 
 if selected == "SQL Formatter":
